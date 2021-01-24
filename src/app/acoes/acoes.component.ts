e018b1193d94896f1a2cf5b861538db9e35f2b5e
MatheusEli <matheuselisouzaferreira@hotmail.com>
2021-01-24 13:51:40 -0300

    implementando distinctUntilChanged e finalizando projeto

--- src/app/acoes/acoes.component.ts
+++ src/app/acoes/acoes.component.ts
@@ -1,22 +1,25 @@
 import { AcoesService } from './acoes.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { filter, switchMap, tap } from 'rxjs/operators';
+import { debounceTime, distinctUntilChanged, filter, switchMap, tap } from 'rxjs/operators';
 import { merge } from 'rxjs';
 
+const ESPERA_DIGITACAO = 300;
 @Component({
   selector: 'app-acoes',
   templateUrl: './acoes.component.html',
   styleUrls: ['./acoes.component.css'],
 })
+
 export class AcoesComponent{
 
   acoesInput = new FormControl();
-
   todasAcoes$ = this.acoesService.getAcoes().pipe(tap(()=>console.log('Fluxo Inicial')));
 
   filtroPeloInput$ = this.acoesInput.valueChanges.pipe(
+  debounceTime(ESPERA_DIGITACAO),
   filter((valorDigitado) => valorDigitado.length >= 3 || !valorDigitado.length),
+  distinctUntilChanged(),
   switchMap((valorDigitado) => this.acoesService.getAcoes(valorDigitado)),
   tap(()=>console.log('Fluxo do Filtro')));
 
